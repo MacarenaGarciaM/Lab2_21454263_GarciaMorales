73dacecf4f91e9cb4c20940689108d25c9effa94
MacarenaGarciaM <145171386+MacarenaGarciaM@users.noreply.github.com>
2024-05-15 22:28:59 -0400

    Implementación

--- main_21454263_GarciaMorales.pl
+++ main_21454263_GarciaMorales.pl
@@ -1,9 +1,17 @@
 %RF2-Constructor
 %station/5
-% Descripcion:Predicado constructor de una estación de metro, las que
-% pueden ser estaciones del tipo: regular, mantención, combinación o
+% Descripcion:Predicado constructor de una estaciÃ³n de metro, las que
+% pueden ser estaciones del tipo: regular, mantenciÃ³n, combinaciÃ³n o
 % terminal.
 %Dom:id (int) X name (String)  X type (stationType) X stopTime (positive integer) X Station
 %Meta Primaria:station/5
 %Meta Secundaria:
 station(Id, Name, Type, StopTime, [Id, Name, Type, StopTime]).
+
+%RF3-Constructor
+%section/5
+%Descripcion: Predicado que permite establecer enlaces entre dos estaciones.
+%Dom: point1 (station)  X point2 (station) X distance (positive-number) X cost (positive-number U {0}) X Section
+%Meta Primaria: Section/5
+%Meta Secundaria:
+section(Point1, Point2, Distance, Cost, [Point1, Point2, Distance, Cost]).
